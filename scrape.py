dc10fd8d76b73ad32a97d71f34e01becfea8d899
Natasha Narasimhan <natasha.narasimhan@gmail.com>
2024-10-08 16:29:25 -0400

    Able to get data from the next page

--- scrape.py
+++ scrape.py
@@ -1,10 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
-import os
+# import os
 import csv
 
 # Enter the URL of the site you are scraping
-URL="https://quotes.toscrape.com/"
+URL="https://quotes.toscrape.com"
 r=requests.get(URL)
 
 soup = BeautifulSoup(r.content, 'html5lib')
@@ -24,9 +24,31 @@ for x in table:
     quotes.append(quote)
     num +=1
 
+# gets the end of the url for the next page
 getNextQuotes = soup.findAll("li", attrs={"class": "next"})
+count = str(2)
 for link in getNextQuotes:
-    print(link.find("a"))
+    # We're getting the URL of the next page
+    tailURL = link.find("a")["href"]
+
+# we concatenate it with our original URL
+tailURL = URL + tailURL
+
+r2 = requests.get(tailURL)
+soup2 = BeautifulSoup(r2.content, "html5lib")
+print(soup2.prettify)
+
+# Note: find only finds the first div specified; use findAll to get all of them.
+table = soup2.findAll("div", attrs={"class": "quote"})
+num = 1
+for x in table:
+    quote = {}
+    quote["author"] = x.find("small", attrs={"class": "author"}).string
+    quote["lines"] = x.find("span", attrs={"class": "text"}).string
+    #quote["author"] = x.find("small", attrs={"class": "author"}).string
+    print(num, ": ", quote["lines"], " by ", quote["author"])
+    quotes.append(quote)
+    num +=1
 
 # Writing to csv files
 fields = ["lines", "author"]
