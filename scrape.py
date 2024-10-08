5a71f1d6d20f2877cf41321a9b86e671232d0be9
Natasha Narasimhan <natasha.narasimhan@gmail.com>
2024-10-08 10:07:46 -0400

    Extracting quotes and authors into a list of dictionaries

--- scrape.py
+++ scrape.py
@@ -1,5 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
+import csv
+# Delete later: import tabulate
 
 # Enter the URL of the site you are scraping
 URL="https://quotes.toscrape.com/"
@@ -9,3 +11,19 @@ soup = BeautifulSoup(r.content, 'html5lib')
 # To get a better formatted visual representation:
 print(soup.prettify)
 
+quotes=[]
+# Note: find only finds the first div specified; use findAll to get all of them.
+table = soup.findAll("div", attrs={"class": "quote"})
+num = 1
+for x in table:
+    quote = {}
+    quote["lines"] = x.find("span", attrs={"class": "text"}).string
+    quote["author"] = x.find("small", attrs={"class": "author"}).string
+    print(num, ": ", quote["lines"], " by ", quote["author"])
+    quotes.append(quote)
+    num +=1
+
+# Just for checking, delete later
+# header = quotes[0].keys()
+# rows =  [x.values() for x in quotes]
+# print(tabulate.tabulate(rows, header))
