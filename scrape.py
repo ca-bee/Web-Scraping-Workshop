c6c753cf281be7851009df52476d70821391f6b1
Natasha Narasimhan <natasha.narasimhan@gmail.com>
2024-10-08 09:01:05 -0400

    Added URL

--- scrape.py
+++ scrape.py
@@ -2,10 +2,10 @@ import requests
 from bs4 import BeautifulSoup
 
 # Enter the URL of the site you are scraping
-URL=""
+URL="https://quotes.toscrape.com/"
 r=requests.get(URL)
 
 soup = BeautifulSoup(r.content, 'html5lib')
 # To get a better formatted visual representation:
-# print(soup.prettify)
+print(soup.prettify)
 
